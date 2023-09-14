141c110c7f29c5e74947c1512fe1201fac972867
Paul Lu <paullu@cs.ualberta.ca>
2023-09-14 09:34:57 -0600

    Paul

---
+++ Week.02/lec04base.py
@@ -0,0 +1,21 @@
+catA = 0
+catB = 0
+catC = 0
+count = 0
+# nextCategory = None
+resp = [ 'A', 'A', 'A', 'B', 'B' ]
+
+# while nextCategory != "Q":
+for nextCategory in resp:
+#    nextCategory = input("Response: ")
+#    count += 1
+    if nextCategory == "A":
+        catA = catA + 1
+    elif nextCategory == "B":
+        catB = catB + 1
+    elif nextCategory == "C":
+        catC = catC + 1
+# count -= 1
+count = len(resp)
+print(catA, catB, catC)
+print(catA/count *100, catB/count *100, catC/count *100)
