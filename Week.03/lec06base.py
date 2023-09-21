16d7cd7c05f7445764bf8f1430dfc86a05b82b92
Paul Lu <paullu@ualberta.ca>
2023-09-21 09:10:33 -0600

    Paul

---
+++ Week.03/lec06base.py
@@ -0,0 +1,22 @@
+import random
+
+def uniqueRandomInt(l,u,target):
+    assert (u+l-1) >= target, "Not enough range"
+    count = 0
+    theNums = []
+    while count < target:
+        n = random.randint(l,u)
+        if n in theNums:
+            pass
+        else:
+            theNums.append(n)
+            count += 1
+    return theNums
+
+if __name__ == "__main__":
+    random.seed(10)
+    print(uniqueRandomInt(0,3,2))
+    print(uniqueRandomInt(6,15,5))
+    print(uniqueRandomInt(1,50,5))
+
+    # WRONG CALLING SEMANTICS print(uniqueRandomInt(6,15,50))
