dfe7cd04daddd81b358b7b9229762e13baf8a958
Paul Lu <paullu@cs.ualberta.ca>
2023-10-03 10:01:21 -0600

    Paul

---
+++ Week.05/lec09p01v02.soln.py
@@ -0,0 +1,11 @@
+def map_bucket(x):
+    buckets = [0.2, 1.0]
+    for i in range(len(buckets)):
+        if x < buckets[i]:
+            return(i)
+    return(-1)  # Error
+
+if __name__ ==  "__main__":
+    print(map_bucket(0.36))
+    print(map_bucket(0.81))
+    print(map_bucket(0.1))
