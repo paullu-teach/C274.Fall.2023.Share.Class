ac7c31197d2bab1f85e467b09e8c843b917c1ec9
Paul Lu <paullu@cs.ualberta.ca>
2023-10-10 06:04:29 -0600

    Paul

---
+++ Week.06/cubicloop.py
@@ -0,0 +1,8 @@
+# Follow-up to Worksheet from Lecture 10, Big-O
+n = 3
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            print("->", k)
+            k = 2 + 2
+            print("  ", k)
