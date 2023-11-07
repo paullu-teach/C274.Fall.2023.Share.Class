833893365e47606fc1ed1c893c616c3106bd684f
Paul Lu <paullu@cs.ualberta.ca>
2023-11-06 20:47:11 -0700

    Paul

---
+++ Week.10/driver01.py
@@ -0,0 +1,14 @@
+from xydata import *
+
+# Only text output
+def main():
+    # s = XYData("Output/driver01")
+    s = XYData("driver01")
+    print(s)
+    s.x([1,2,3,4])
+    s.y([2,4,6,8])
+    print(s.x())
+    print(s.y())
+
+if __name__ == "__main__":
+    main()
---
+++ Week.10/xydata.py
@@ -0,0 +1,51 @@
+# Copyright 2023 Paul Lu
+
+import matplotlib.pyplot as xyplt
+
+class XYData:
+    def __init__(self,name=""):
+        self.__x = []
+        self.__y = []
+        self.__name = name
+        return
+
+    def __str__(self):
+        s = "XYData: " + self.name()
+        return s
+
+    def __repr__(self):
+        s = "Id<"+ str(id(self)) + "> " + self.name()
+        return s
+
+    def name(self):
+        return(self.__name)
+
+    def x(self,data=[]):
+        # HINT: Borrow idea from Lecture 14 worksheet, polymorphism
+        if len(data) == 0:
+            return(self.__x)
+        elif type(data) is list:
+            self.__x = data.copy()
+            assert self.__x is not data, "Not copied"
+            return(len(data))
+
+        assert False, "Should not be here: x"
+        return None
+
+    def y(self,data=[]):
+        # HINT: Borrow idea from Lecture 14 worksheet, polymorphism
+        if len(data) == 0:
+            return(self.__y)
+        elif type(data) is list:
+            self.__y = data.copy()
+            assert self.__y is not data, "Not copied"
+            return(len(data))
+
+        assert False, "Should not be here: y"
+        return None
+
+    def plotpng(self):
+        xyplt.figure(figsize=(8, 4))
+        xyplt.scatter(self.x(), self.y(), label=self.name())
+        xyplt.legend([" x "], loc="upper left")
+        xyplt.savefig(self.name() + ".png")
